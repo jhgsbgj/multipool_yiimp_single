142ce3631f83a90733de05047904b4bc01b94d21
Original-Tasty <quentin.neveu@hotmail.ca>
2021-03-01 17:39:43 -0500

    php memcache fix

--- system.sh
+++ system.sh
@@ -115,6 +115,7 @@ php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi \
 php-pear php-auth-sasl mcrypt imagemagick libruby \
 php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 \
 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache \
+php7.2-memcache php7.2-memcached memcached \
 php-imagick php-gettext php7.2-zip php7.2-mbstring \
 fail2ban ntpdate python3 python3-dev python3-pip \
 curl git sudo coreutils pollinate unzip unattended-upgrades cron \
